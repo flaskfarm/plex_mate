95b7ab711090fe6991efa7d12ed83c1fa5cf7525
kihyyo <83176214+kihyyo@users.noreply.github.com>
2025-06-10 17:21:37 +0900

    Update mod_scan.py

--- mod_scan.py
+++ mod_scan.py
@@ -23,6 +23,8 @@ class ModuleScan(PluginModuleBase):
             f"{self.name}_filecheck_thread_interval": "60",
             f"{self.name}_db_delete_day": "30",
             f"{self.name}_db_auto_delete": "True",
+            f"{self.name}_use_web_request": "False",
+            f"{self.name}_web_sections": "",
             f"{self.name}_use_vfs_refresh": "False",
             f"{self.name}_vfs_change_rule": "/mnt/gds2/GDRIVE|/GDRIVE|172.17.0.1:5275\n/mnt/mydrive|/sample/172.17.0.1:5524",
             f"{self.name}_max_scan_time": "60",
