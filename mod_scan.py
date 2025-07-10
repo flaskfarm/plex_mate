abf35b683b242efa0b1b69a06d798f2c10f59afd
halfaider <castwhendamagedealt@gmail.com>
2025-07-10 10:57:30 +0900

    추가: 웹 스캔 요청 API, vfs/refresh API 수정: vfs/refresh 실행 조건에서 경로 검사 삭제

--- mod_scan.py
+++ mod_scan.py
@@ -1,7 +1,9 @@
+import threading
+
 from .model_scan import ModelScanItem
 from .setup import *
 from .task_scan import Task
-from .extensions import check_timeover, BrowserPage, TrashPage
+from .extensions import check_timeover, BrowserPage, TrashPage, vfs_refresh
 from .plex_web import PlexWebHandle
 
 name = 'scan'
@@ -55,22 +57,43 @@ class ModuleScan(PluginModuleBase):
     def process_api(self, sub, req):
         ret = {'ret':'success'}
         if sub == 'do_scan':
-            #P.logger.warning(d(req.form))
-            ModelScanItem(
-                req.form['target'],
-                mode = req.form.get('mode', 'ADD'),
-                target_section_id = req.form.get('target_section_id', '0'),
-                callback_id = req.form.get('callback_id'),
-                callback_url = req.form.get('callback_url'),
-            ).save()
-
+            target = req.form['target']
+            target_section_id = req.form.get('target_section_id') or 0
+            mode = req.form.get('mode') or 'ADD'
+            callback_id = req.form.get('callback_id')
+            callback_url = req.form.get('callback_url')
+            scanner = req.form.get('scanner')
+            if scanner == 'web':
+                th = threading.Thread(target=PlexWebHandle.path_scan, args=(target_section_id, target))
+                th.daemon = True
+                th.start()
+                ret['msg'] = f'{target_section_id=} {scanner=} {target=}'
+            else:
+                #P.logger.warning(d(req.form))
+                ModelScanItem(
+                    target,
+                    mode = mode,
+                    target_section_id = target_section_id,
+                    callback_id = callback_id,
+                    callback_url = callback_url,
+                ).save()
+                ret['msg'] = f'{mode=} {target=}'
         elif sub == 'manual_refresh':
             meta_id = req.form.get('metadata_item_id')
             if not meta_id:
                 return jsonify({'ret': 'fail', 'msg': 'id 가 존재하지 않습니다.'})
             ret = {'ret':'success', 'msg': f'{meta_id} 수신 성공'}
             PlexWebHandle.manual_refresh(meta_id, plugin_instance=self)
-
+        elif sub == 'vfs_refresh':
+            target = req.form.get('target')
+            recursive = (req.form.get('recursive') == 'true') or False
+            async_ = (req.form.get('async') == 'true') or False
+            th = threading.Thread(target=vfs_refresh, args=(target, recursive, async_))
+            th.daemon = True
+            th.start()
+            ret['msg'] = f'{recursive=} async={async_} {target=}'
+        else:
+            return {'ret':'fail', 'msg':'Bad request'}, 400
         return jsonify(ret)
 
 
